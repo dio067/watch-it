64bde8e1667c7f3cfa412dc1df261eef7fffabba
dio067 <yousefnas06@gmail.com>
2025-04-18 19:06:07 +0400

    chore: apply Prettier formatting and remove unused code throughout root dir

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 #! /usr/bin/env node
-const chokidar = require('chokidar');
-const debounce = require('lodash.debounce');
-const prog = require('caporal');
-const fs = require('fs');
-const { spawn } = require('child_process');
+const chokidar = require("chokidar");
+const debounce = require("lodash.debounce");
+const prog = require("caporal");
+const fs = require("fs");
+const { spawn } = require("child_process");
 
 prog
-.version('1.0.1')
-.argument('[filename]', 'Name of the file to excute')
-.action( async ({ filename }) => {
+	.version("1.0.1")
+	.argument("[filename]", "Name of the file to excute")
+	.action(async ({ filename }) => {
+		const name = filename;
 
-    const name = filename;
+		try {
+			await fs.promises.access(name);
+		} catch {
+			throw new Error(`Could not find the file ${name}`);
+		}
 
-    try {
-     await fs.promises.access(name);
-    } catch {
-        throw new Error(`Could not find the file ${name}`);
-    }
+		let proc;
+		const start = debounce(() => {
+			if (proc) {
+				proc.kill();
+			}
+			proc = spawn("node", [`${name}`], { stdio: "inherit" });
+		}, 100);
 
-    const start = debounce(() => {
-        spawn('node', [`${name}`], {stdio: 'inherit'});
-    }, 100);        
-    
-    chokidar.watch('.')
-    .on('add', start)
-    .on('change', start)
-    .on('unlink', start);
-});
+		chokidar
+			.watch(".")
+			.on("add", start)
+			.on("change", start)
+			.on("unlink", start);
+	});
 prog.parse(process.argv);
-
