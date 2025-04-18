927f590828aee297b3519fa7cb91ca608f0ba464
dio067 <yousefnas06@gmail.com>
2025-04-18 17:07:46 +0400

    feat: ensuring file exist

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-#!usr/bin/env node
+#! /usr/bin/env node
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
+const prog = require('caporal');
+const fs = require('fs');
 
-const start = debounce(() => {
-    console.log("STARTING THE PROGRAM!!");
-}, 100);
+prog
+.version('1.0.1')
+.argument('[filename]', 'Name of the file to excute')
+.action( async ({ filename }) => {
 
-chokidar.watch('.')
-.on('add', start)
-.on('change', () => console.log("file changed"))
-.on('unlink', () => console.log("file removed"));
-console.log('hello there');         
+    const name = filename;
+
+    try {
+     await fs.promises.access(name);
+    } catch {
+        throw new Error(`Could not find the file ${name}`);
+    }
+
+    const start = debounce(() => {
+        console.log("STARTING THE PROGRAM!!");
+    }, 100);
+    
+    chokidar.watch('.')
+    .on('add', start)
+    .on('change', start)
+    .on('unlink', start);
+});
+prog.parse(process.argv);
+
